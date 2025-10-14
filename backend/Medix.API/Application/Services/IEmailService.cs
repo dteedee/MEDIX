a323f0e450e0cd17766f71a83acd6640d5344b7a
teedee <tungduongvbhp@gmail.com>
2025-10-14 19:28:55 +0700

    update

--- backend/Medix.API/Application/Services/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-namespace Medix.API.Application.Services
-{
-    public interface IEmailService
-    {
-        Task SendPasswordResetEmailAsync(string email, string resetToken);
-        Task SendEmailVerificationAsync(string email, string verificationToken);
-    }
-}
-

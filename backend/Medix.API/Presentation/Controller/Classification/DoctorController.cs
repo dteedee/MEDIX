88da76180b976bd907f6f249a0d2bf146b800fcd
boyfinger <122414066+boyfinger@users.noreply.github.com>
2025-10-18 21:49:13 +0700

    putting doctor register validation together

--- backend/Medix.API/Presentation/Controller/Classification/DoctorController.cs
+++ backend/Medix.API/Presentation/Controller/Classification/DoctorController.cs
@@ -1,10 +1,14 @@
-﻿using Medix.API.Application.DTOs.Doctor;
+﻿using AutoMapper;
+using Medix.API.Application.DTOs.Doctor;
 using Medix.API.Business.Interfaces.Classification;
 using Medix.API.Business.Interfaces.UserManagement;
 using Medix.API.Business.Services.Community;
 using Medix.API.Models.DTOs.Doctor;
 using Medix.API.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Org.BouncyCastle.Ocsp;
+using System.ComponentModel.DataAnnotations;
 
 namespace Medix.API.Presentation.Controller.Classification
 {
@@ -16,14 +20,16 @@ namespace Medix.API.Presentation.Controller.Classification
         private readonly ISpecializationService _specializationService;
         private readonly CloudinaryService _cloudinaryService;
         private readonly IUserService _userSerivce;
+        private readonly IMapper _mapper;
 
         public DoctorController(IDoctorService doctorService, ISpecializationService specializationService,
-            CloudinaryService cloudinaryService, IUserService userService)
+            CloudinaryService cloudinaryService, IUserService userService, IMapper mapper)
         {
             _doctorService = doctorService;
             _specializationService = specializationService;
             _cloudinaryService = cloudinaryService;
             _userSerivce = userService;
+            _mapper = mapper;
         }
 
         [HttpGet("register-metadata")]
@@ -42,12 +48,25 @@ namespace Medix.API.Presentation.Controller.Classification
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> RegisterDoctor([FromForm] DoctorRegisterRequest request)
+        public async Task<IActionResult> RegisterDoctor([FromForm] DoctorRegisterPresenter presenter)
         {
-            await ValidateAsync(request);
-            if (!ModelState.IsValid)
+            var request = _mapper.Map<DoctorRegisterRequest>(presenter);
+            var validationResults = new List<ValidationResult>();
+            var context = new ValidationContext(request, null, null);
+
+            Validator.TryValidateObject(request, context, validationResults, true);
+            validationResults = await ValidateAsync(request, validationResults);
+            if (validationResults.Any())
             {
-                return ValidationProblem(ModelState);
+                var modelState = new ModelStateDictionary();
+                foreach (var validationResult in validationResults)
+                {
+                    foreach (var memberName in validationResult.MemberNames)
+                    {
+                        modelState.AddModelError(memberName, validationResult.ErrorMessage ?? "Invalid value");
+                    }
+                }
+                return ValidationProblem(modelState);
             }
 
             User user = new User
@@ -98,32 +117,34 @@ namespace Medix.API.Presentation.Controller.Classification
             return Ok(new { Message = "Doctor registered successfully" });
         }
 
-        private async Task ValidateAsync(DoctorRegisterRequest request)
+        private async Task<List<ValidationResult>> ValidateAsync(DoctorRegisterRequest request, List<ValidationResult> prev)
         {
-            if (await _userSerivce.EmailExistsAsync(request.Email))
+            if (request.Email != null && await _userSerivce.EmailExistsAsync(request.Email))
             {
-                ModelState.AddModelError("Email", "Email đã được sử dụng");
+                prev.Add(new ValidationResult("Email đã được sử dụng", new[] { "Email" }));
             }
 
-            if (await _userSerivce.PhoneNumberExistsAsync(request.PhoneNumber))
+            if (request.PhoneNumber != null && await _userSerivce.PhoneNumberExistsAsync(request.PhoneNumber))
             {
-                ModelState.AddModelError("PhoneNumber", "Số điện thoại đã được sử dụng");
+                prev.Add(new ValidationResult("Số điện thoại đã được sử dụng", new[] { "PhoneNumber" }));
             }
 
-            if (await _doctorService.LicenseNumberExistsAsync(request.LicenseNumber))
+            if (request.LicenseNumber != null && await _doctorService.LicenseNumberExistsAsync(request.LicenseNumber))
             {
-                ModelState.AddModelError("LicenseNumber", "Số giấy phép hành nghề đã được sử dụng");
+                prev.Add(new ValidationResult("Số giấy phép hành nghề đã được sử dụng", new[] { "LicenseNumber" }));
             }
 
-            if (await _userSerivce.UserNameExistsAsync(request.UserName))
+            if (request.UserName != null && await _userSerivce.UserNameExistsAsync(request.UserName))
             {
-                ModelState.AddModelError("UserName", "Tên đăng nhập đã được sử dụng");
+                prev.Add(new ValidationResult("Tên đăng nhập đã được sử dụng", new[] { "UserName" }));
             }
 
-            if (await _userSerivce.IdentificationNumberExistsAsync(request.IdentificationNumber))
+            if (request.IdentificationNumber != null && await _userSerivce.IdentificationNumberExistsAsync(request.IdentificationNumber))
             {
-                ModelState.AddModelError("IdentificationNumber", "Số CCCD/CMND đã được sử dụng");
+                prev.Add(new ValidationResult("Số CCCD/CMND đã được sử dụng", new[] { "IdentificationNumber" }));
             }
+
+            return prev;
         }
     }
 }

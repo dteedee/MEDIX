71ec7087035504e7ea373af72f938d1c6e894cce
LinhHaNam <linhfromhanam03@gmail.com>
2025-10-24 10:56:10 +0700

    .

--- backend/Medix.API/Program.cs
+++ backend/Medix.API/Program.cs
@@ -111,24 +111,7 @@ builder.Services.AddSwaggerGen(c =>
 // AutoMapper is already configured in ServiceConfiguration.cs
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IJwtService, JwtService>();
-builder.Services.AddScoped<IEmailService, EmailService>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
-builder.Services.AddScoped<IUserRoleRepository, UserRoleRepository>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IPatientRepository, PatientRepository>();
-builder.Services.AddScoped<ICmspageRepository, CmspageRepository>();
-builder.Services.AddScoped<ICmspageService, CmspageService>();
-builder.Services.AddScoped<IHealthArticleRepository, HealthArticleRepository>();
-builder.Services.AddScoped<IHealthArticleService, HealthArticleService>();
-builder.Services.AddScoped<IContentCategoryRepository, ContentCategoryRepository>();
-builder.Services.AddScoped<IContentCategoryService, ContentCategoryService>();
-builder.Services.AddScoped<ISiteBannerRepository,SiteBannerRepository>();
-builder.Services.AddScoped<ISiteBannerService, SiteBannerService>();
-builder.Services.AddScoped<IDtoValidatorService, DtoValidatorService>();
-builder.Services.AddScoped<CloudinaryService>();
+builder.Services.ConfigureServices();
 
 
 

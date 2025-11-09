00ff3f5d764d4a3a6770d848247719fe3b365cc3
teedee <tungduongvbhp@gmail.com>
2025-11-09 15:46:30 +0700

    Update Program.cs

--- backend/Medix.API/Program.cs
+++ backend/Medix.API/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using Net.payOS;
+using PayOS;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);

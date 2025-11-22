d867a13103f4cb798adfd5de6eebacff4cb519ff
LinhHaNam <linhfromhanam03@gmail.com>
2025-11-23 01:53:41 +0700

    .fix

--- backend/Medix.API/Presentation/Controller/Classification/DashboardController.cs
+++ backend/Medix.API/Presentation/Controller/Classification/DashboardController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Security.Claims;
 using Medix.API.Business.Interfaces.Classification;
 using Microsoft.AspNetCore.Authorization;
-﻿using Medix.API.Business.Interfaces.Classification;
+using Medix.API.Business.Interfaces.Classification;
 using Medix.API.Business.Interfaces.UserManagement;
 using Medix.API.Business.Services.Classification;
 using Microsoft.AspNetCore.Http;
@@ -14,24 +14,20 @@ namespace Medix.API.Presentation.Controller.Classification
     [Route("api/[controller]")]
     public class DashboardController : ControllerBase
     {
+
         private readonly IDoctorDashboardService _service;
         private readonly IAdminDashboardService _adminService;
-
-        public DashboardController(IDoctorDashboardService service, IAdminDashboardService adminService)
-        {
-            _service = service;
-            _adminService = adminService;
-        }
         private readonly ISpecializationService _specializationService;
         private readonly IAppointmentService appointmentService;
         private readonly IUserService _userService;
 
-        public DashboardController(IDoctorDashboardService service, ISpecializationService specializationService, IAppointmentService appointmentService, IUserService userService)
+        public DashboardController(IDoctorDashboardService service, ISpecializationService specializationService, IAppointmentService appointmentService, IUserService userService, IAdminDashboardService adminService)
         {
-            _service = service;
             _specializationService = specializationService;
             this.appointmentService = appointmentService;
             _userService = userService;
+            _service = service;
+            _adminService = adminService;
         }
 
         [HttpGet("doctor/{doctorId}")]
